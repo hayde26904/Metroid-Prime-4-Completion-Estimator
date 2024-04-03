d8553e25f839adb629fe0c9c957b9a81dc1eaa1c
hayde26904 <56613295+hayde26904@users.noreply.github.com>
2024-04-03 08:39:33 -0400

    Removed unnecessary request headers.

--- index.js
+++ index.js
@@ -25,23 +25,7 @@ function filterListings(listings, word) {
 
 async function getJobListings(listingsURL) {
     try {
-        const res = await axios.get(listingsURL, {
-            headers: {
-                "accept": "application/json, text/javascript, */*; q=0.01",
-                "accept-language": "en-US,en;q=0.5",
-                "if-modified-since": "Thu, 28 Mar 2024 00:48:43 GMT",
-                "if-none-match": "\"155a0459-e611-4bcd-a8e4-8e7f5b7b2b1e\"",
-                "sec-ch-ua": "\"Brave\";v=\"123\", \"Not:A-Brand\";v=\"8\", \"Chromium\";v=\"123\"",
-                "sec-ch-ua-mobile": "?0",
-                "sec-ch-ua-platform": "\"Windows\"",
-                "sec-fetch-dest": "empty",
-                "sec-fetch-mode": "cors",
-                "sec-fetch-site": "cross-site",
-                "sec-gpc": "1",
-                "Referer": "https://careers.nintendo.com/",
-                "Referrer-Policy": "strict-origin-when-cross-origin"
-            }
-        });
+        const res = await axios.get(listingsURL, {});
 
         return res.data;
     } catch (error) {
